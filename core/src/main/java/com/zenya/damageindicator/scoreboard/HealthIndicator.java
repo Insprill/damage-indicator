51e555fa43a14da6b30dc5d616ec81d74c143861
Insprill <insprill@gmail.com>
2021-09-01 17:04:58 -0400

    Cleanup HealthIndicator

--- core/src/main/java/com/zenya/damageindicator/scoreboard/HealthIndicator.java
+++ core/src/main/java/com/zenya/damageindicator/scoreboard/HealthIndicator.java
@@ -9,8 +9,10 @@ import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class HealthIndicator {
+
     public static HealthIndicator INSTANCE = new HealthIndicator();
-    private Scoreboard board;
+
+    private final Scoreboard board;
     private Objective obj;
 
     private HealthIndicator() {
@@ -20,28 +22,32 @@ public class HealthIndicator {
     }
 
     public void updateHealth(Player player) {
-        if(obj != null) obj.getScore(player.getName()).setScore((int) player.getHealth());
+        if (obj != null)
+            obj.getScore(player.getName()).setScore((int) player.getHealth());
     }
 
     public void reload() {
-        if(StorageFileManager.getConfig().getBool("health-indicators")) {
-            //Enable health indicators
-            if(obj == null) {
-                //Init if not exists
-                try {
-                    obj = board.registerNewObjective("di-health", "health");
-                } catch(NullPointerException exc) {
-                    //Depreciation
-                    if(obj != null) obj.unregister();
-                    obj = board.registerNewObjective("di-health", "health", ChatColor.translateAlternateColorCodes('&', StorageFileManager.getMessages().getString("health")));
-                }
-                obj.setDisplaySlot(DisplaySlot.BELOW_NAME);
-            }
-            //Update displayname regardless
-            obj.setDisplayName(ChatColor.translateAlternateColorCodes('&', StorageFileManager.getMessages().getString("health")));
-        } else {
+        if (!StorageFileManager.getConfig().getBool("health-indicators")) {
             //Disable health indicators
-            if(obj != null) obj.unregister();
+            if (obj != null) {
+                obj.unregister();
+                obj = null;
+            }
+            return;
         }
+        //Enable health indicators
+        if (obj == null) {
+            //Init if not exists
+            try {
+                obj = board.registerNewObjective("di-health", "health");
+            } catch (NullPointerException exc) {
+                //Depreciation
+                obj = board.registerNewObjective("di-health", "health", ChatColor.translateAlternateColorCodes('&', StorageFileManager.getMessages().getString("health")));
+            }
+            obj.setDisplaySlot(DisplaySlot.BELOW_NAME);
+        }
+        //Update displayname regardless
+        obj.setDisplayName(ChatColor.translateAlternateColorCodes('&', StorageFileManager.getMessages().getString("health")));
     }
+
 }
