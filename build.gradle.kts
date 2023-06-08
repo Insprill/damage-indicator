bf96b73be4716aeeb42200c36ea59b828f234e4b
Pierce Thompson <insprill@gmail.com>
2023-06-08 16:43:13 -0400

    Add support for 1.20, bump paperweight to 1.5.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(project(":nms:v1_19_R1", "reobf"))
     implementation(project(":nms:v1_19_R2", "reobf"))
     implementation(project(":nms:v1_19_R3", "reobf"))
+    implementation(project(":nms:v1_20_R1", "reobf"))
 }
 
 tasks {
--- nms/v1_20_R1/build.gradle.kts
+++ nms/v1_20_R1/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     compileOnly(project(":core"))
-    paperweight.paperDevBundle("1.18-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.20-R0.1-SNAPSHOT")
 }
 
 tasks {
