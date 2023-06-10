e97244694a09c6001ae26f2a370652765442ad5c
Pierce Thompson <insprill@gmail.com>
2023-06-09 23:16:32 -0400

    Allow Gradle to automatically download toolchains

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,5 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
-    }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
 rootProject.name = "damage-indicator"
