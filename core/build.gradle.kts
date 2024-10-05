78995a8dd000c4eabfd445e122c9673fd96ad3a1
Pierce Thompson <pierce@insprill.net>
2024-10-05 03:57:31 -0400

    Update deps

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,11 +9,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:24.1.0")
+    compileOnly("org.jetbrains:annotations:25.0.0")
     compileOnly("org.spigotmc:spigot-api:1.20.3-R0.1-SNAPSHOT")
     implementation("net.insprill:spigot-utils:0.4.0")
     implementation("net.insprill:XenLib:d7d95983fe")
-    implementation("org.bstats:bstats-bukkit:3.0.2")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
 }
 
 tasks {
