80b5f7e6819633e7721a199462afdc6283f647e0
Pierce Thompson <pierce@insprill.net>
2025-07-12 17:21:53 -0400

    Add Modrinth publish workflow, update setup-gradle cache rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
-import java.io.ByteArrayOutputStream
-
 plugins {
     id("java")
+    id("org.ajoberstar.grgit") version "5.3.0"
     id("com.gradleup.shadow") version "9.0.0-beta17"
     id("com.rikonardo.papermake") version "1.0.6"
+    id("com.modrinth.minotaur") version "2.8.7"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17" apply false
 }
 
 group = "net.insprill"
-version = this.getFullVersion()
+version = "${project.version}${versionMetadata()}"
 
 allprojects {
     group = project.group
@@ -73,20 +73,121 @@ tasks {
     }
 }
 
-fun getFullVersion(): String {
-    val version = project.property("version")!! as String
-    return if (version.contains("-SNAPSHOT")) {
-        "$version+rev.${getGitHash()}"
-    } else {
-        version
-    }
+val minecraftVersions = arrayOf(
+    "1.8",
+    "1.8.1",
+    "1.8.2",
+    "1.8.3",
+    "1.8.4",
+    "1.8.5",
+    "1.8.6",
+    "1.8.7",
+    "1.8.8",
+    "1.9",
+    "1.9.1",
+    "1.9.2",
+    "1.9.3",
+    "1.9.4",
+    "1.10",
+    "1.10.1",
+    "1.10.2",
+    "1.11",
+    "1.11.1",
+    "1.11.2",
+    "1.12",
+    "1.12.1",
+    "1.12.2",
+    "1.13",
+    "1.13.1",
+    "1.13.2",
+    "1.14",
+    "1.14.1",
+    "1.14.2",
+    "1.14.3",
+    "1.14.4",
+    "1.15",
+    "1.15.1",
+    "1.15.2",
+    "1.16",
+    "1.16.1",
+    "1.16.2",
+    "1.16.3",
+    "1.16.4",
+    "1.16.5",
+    "1.17",
+    "1.17.1",
+    "1.18",
+    "1.18.1",
+    "1.18.2",
+    "1.19",
+    "1.19.1",
+    "1.19.2",
+    "1.19.3",
+    "1.19.4",
+    "1.20",
+    "1.20.1",
+    "1.20.2",
+    "1.20.3",
+    "1.20.4",
+    "1.20.5",
+    "1.20.6",
+    "1.21",
+    "1.21.1",
+    "1.21.2",
+    "1.21.3",
+    "1.21.4",
+    "1.21.5",
+    "1.21.6",
+    "1.21.7"
+)
+
+modrinth {
+    changelog.set(readChangelog(project.version as String))
+    token.set(System.getenv("MODRINTH_API_TOKEN") ?: findProperty("modrinthToken") as String?)
+    projectId.set(property("modrinth.project.id") as String)
+    versionType.set(if ((findProperty("build.is-release") as String? ?: "true").toBoolean()) "release" else "alpha")
+    uploadFile.set(tasks.shadowJar.get())
+    loaders.addAll("spigot", "paper", "purpur")
+    syncBodyFrom.set(file("modrinth_page.md").readText())
+    gameVersions.addAll(*minecraftVersions)
 }
 
-fun getGitHash(): String {
-    val stdout = ByteArrayOutputStream()
-    exec {
-        commandLine("git", "rev-parse", "--verify", "--short", "HEAD")
-        standardOutput = stdout
+fun readChangelog(version: String): String {
+    val lines = file("CHANGELOG.md").readLines()
+    val out = StringBuilder()
+    var inVersion = false
+    for (line in lines) {
+        if (line.startsWith("## [$version] - ")) {
+            inVersion = true
+            continue
+        }
+        if (inVersion) {
+            if (line.startsWith("## ")) {
+                break
+            }
+            out.append(line).append("\n")
+        }
     }
-    return stdout.toString().trim()
+
+    if (out.isBlank()) {
+        out.append("[${grgit.head().abbreviatedId}](${grgit.remote.list().first().url}/commit/${grgit.head().id}): ${grgit.head().fullMessage}")
+    }
+
+    return out.toString().trim()
 }
+
+fun versionMetadata(): String {
+    if (!property("version.metadata").toString().toBoolean()) {
+        return ""
+    }
+
+    val head = grgit.head()
+    var id = head.abbreviatedId
+
+    if (!grgit.status().isClean) {
+        id += ".dirty"
+    }
+
+    return "+rev.${id}"
+}
+
